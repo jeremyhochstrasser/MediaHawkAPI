a98771a55e454c1f828bf566974a92cc356998c4
Jeremy Hochstrasser <hochstrasserjeremy@gmail.com>
2023-12-08 19:49:19 -0700

    add hangman api

--- index.js
+++ index.js
@@ -57,6 +57,51 @@ app.post('/api/generate-ideas', async (req, res) => {
     }
 });
 
+app.post('/api/generate-hangman', async (req, res) => {
+    try {
+        // Extracting data from the request body
+        const { wordGenre } = req.body;
+
+        // Construct the prompt for the OpenAI API
+        const prompt = `In 1 word, generate a word to play hangman with that is 8 letters long and relates to the following: ${wordGenre}`;
+
+        // Define the request payload for OpenAI API, including max_tokens
+        const openaiRequestPayload = {
+            model: "gpt-3.5-turbo",
+            messages: [{"role": "user", "content": prompt}],
+            temperature: 0.7,
+            max_tokens: 15 // Set the max_tokens value as needed
+        };
+
+        
+        // Make the API call to OpenAI
+        const response = await fetch('https://api.openai.com/v1/chat/completions', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.OPENAI_API_KEY}` // Use an environment variable for the API key
+            },
+            body: JSON.stringify(openaiRequestPayload)
+        });
+
+        // Check if the response is not okay
+        if (!response.ok) {
+            const errorText = await response.text();
+            console.error("Error response from OpenAI:", errorText);
+            return res.status(500).send(`Error from OpenAI API: ${errorText}`);
+        }
+
+        // Parse the response data
+        const data = await response.json();
+        res.status(200).json(data);
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
